e3a8921f4df1095c569ce5ce47dacaaad4ede36f
gokken-roko <kenta_gotou@kadokawa.jp>
2024-01-29 11:14:16 +0900

    Fix for eslint

--- lib/aws-step-functions-tutorials-stack.ts
+++ lib/aws-step-functions-tutorials-stack.ts
@@ -1,11 +1,11 @@
-import { Construct } from "constructs";
-import * as cdk from "aws-cdk-lib";
-import { Stack, StackProps } from "aws-cdk-lib";
+import { type Construct } from "constructs";
+import { Stack, type StackProps } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsStepFunctionsTutorialsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AwsStepFunctionsTutorialsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    console.log("TODO: Implement stack");
 
     // The code that defines your stack goes here
 
